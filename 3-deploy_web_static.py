71a47b409f16c3c09ecba7244bf66ac0836f5582
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-03-07 20:46:37 -0800

    deploy web_static files to servers

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -24,19 +24,22 @@ def do_deploy(archive_path):
     """Distributes an archive to your web servers"""
     if not os.path.isfile(archive_path):
         return False
-    put(archive_path, "/tmp/")
-    arch_name =  archive_path.split('/')[1].split('.')[0]
-    arch_file = f"{arch_name}.tgz"
-    run(f"mkdir -p /data/web_static/releases/{arch_name}")
+    try:
+        put(archive_path, "/tmp/")
+        arch_name =  archive_path.split('/')[1].split('.')[0]
+        arch_file = f"{arch_name}.tgz"
+        run(f"mkdir -p /data/web_static/releases/{arch_name}")
 
-    # creates /data/web_static/releases/{arch_name}/web_static folder
-    run(f"tar -xzf /tmp/{arch_file} -C /data/web_static/releases/{arch_name}")
-    run(f"rm /tmp/{arch_file}")
-    run(f"mv /data/web_static/releases/{arch_name}/web_static/* /data/web_static/releases/{arch_name}")
-    run(f"rm -rf /data/web_static/releases/{arch_name}/web_static")
-    run("sudo rm /data/web_static/current")
-    run(f"ln -s /data/web_static/releases/{arch_name} /data/web_static/current")
-    return True
+        # creates /data/web_static/releases/{arch_name}/web_static folder
+        run(f"tar -xzf /tmp/{arch_file} -C /data/web_static/releases/{arch_name}")
+        run(f"rm /tmp/{arch_file}")
+        run(f"mv /data/web_static/releases/{arch_name}/web_static/* /data/web_static/releases/{arch_name}")
+        run(f"rm -rf /data/web_static/releases/{arch_name}/web_static")
+        run("sudo rm /data/web_static/current")
+        run(f"ln -s /data/web_static/releases/{arch_name} /data/web_static/current")
+        return True
+    except Exception as e:
+        return False
 
 def deploy():
     """Deploys files tp web servers"""
